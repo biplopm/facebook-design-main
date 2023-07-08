db526ad9c233559ab803f8965afc7fa5c62e13e4
biplopm <kettyhomes6@gmail.com>
2023-07-09 00:28:49 +0600

    commit message

---
+++ app/data.js
@@ -0,0 +1,151 @@
+const posts = [
+  {
+    id: "1",
+    image: "https://shorturl.at/lCGM9",
+    text: "The Art of Inside Out - Disney Pixar",
+    react: "500 Likes",
+    comment: "2.5k Comment",
+    timeSet: "20 min ago",
+    share: {
+      name: "Broc Smith",
+    },
+    author: {
+      name: "Amilton Julio",
+      ima: "https://shorturl.at/enHY0",
+    },
+  },
+  {
+    id: "2",
+    image: "https://shorturl.at/rtwzP",
+    text: "The Art of Megamind - DreamWorks Animation",
+    react: "774 Likes",
+    comment: "800 Comment",
+    share: {
+      name: "Lillian Jonguitud",
+    },
+    author: {
+      name: "Aira M. Emily",
+      ima: "https://shorturl.at/ikrLT",
+    },
+  },
+  {
+    id: "3",
+    image: "https://shorturl.at/cvW38",
+    text: "Happy anniversary to Tarzan",
+    react: "827 Love React",
+    comment: "400 Comment",
+    timeSet: "40 min ago",
+    share: {
+      name: "Anny Flores Sánchez",
+    },
+    author: {
+      name: "Diamond Tran",
+      ima: "https://shorturl.at/klwEZ",
+    },
+  },
+  {
+    id: "4",
+    image: "https://shorturl.at/kGKLT",
+    text: "The Art of Robin Hood - Disney Animation Studios",
+    react: "1.1k Care React",
+    comment: "1.1k Comment",
+    timeSet: "50 min ago",
+    share: {
+      name: "Joe Wierenga",
+    },
+    author: {
+      name: "Steven Awalt",
+      ima: "https://shorturl.at/fiQX2",
+    },
+  },
+  {
+    id: "5",
+    image: "https://shorturl.at/DFGS7",
+    text: "The Art of Puss in Boots - DreamWorks Animation",
+    react: "1.4k Love React",
+    comment: "2.5k Comment",
+    timeSet: "60 min ago",
+    share: {
+      name: "Nicole Childers",
+    },
+    author: {
+      name: "Natalia Gallegos Cárdenas",
+      ima: "https://shorturl.at/lDGW5",
+    },
+  },
+  {
+    id: "6",
+    image: "https://shorturl.at/owK45",
+    text: "The art of Peabody and sherman - DreamWorks Animation",
+    react: "1.8k Wow React",
+    comment: "500 Comment",
+    timeSet: "1 Hour ago",
+    share: {
+      name: "Omie Id",
+    },
+    author: {
+      name: "Christian Brasel",
+      ima: "https://shorturl.at/nV147",
+    },
+  },
+  {
+    id: "7",
+    image: "https://shorturl.at/emBCF",
+    text: "Klaus - Backgrounds - © The SPA Studios",
+    react: "1.9k Wow React",
+    comment: "1.2k Comment",
+    timeSet: "1 Hour 5 min ago",
+    share: {
+      name: "MuĦãmmãđ SĦãyãŋ",
+    },
+    author: {
+      name: "Sierra Alexander",
+      ima: "https://shorturl.at/uL148",
+    },
+  },
+  {
+    id: "8",
+    image: "https://shorturl.at/bxzCO",
+    text: "Pinocchio Concept by Gustaf Tenggren",
+    react: "1.1k HaHa React",
+    comment: "1.8k Comment",
+    timeSet: "1 Hour 10 min ago",
+    share: {
+      name: "Ian Webster",
+    },
+    author: {
+      name: "Willow Hodge",
+      ima: "https://shorturl.at/ilvKL",
+    },
+  },
+  {
+    id: "9",
+    image: "https://shorturl.at/ksC47",
+    text: "The Art of Grinch - Illumination",
+    react: "1.5k HaHa React",
+    comment: "1.6k Comment",
+    timeSet: "1 Hour 15 min ago",
+    share: {
+      name: "Chris Marino",
+    },
+    author: {
+      name: "Kyle Gentry",
+      ima: "https://shorturl.at/czBD1",
+    },
+  },
+  {
+    id: "10",
+    image: "https://shorturl.at/clOS6",
+    text: "The Art of The Pirates",
+    react: "1.6k Likes",
+    comment: "1.5k Comment",
+    timeSet: "1 Hour 30 min ago",
+    share: {
+      name: "Ron Metzle",
+    },
+    author: {
+      name: "Tosca Colautti",
+      ima: "https://shorturl.at/kEV35",
+    },
+  },
+];
